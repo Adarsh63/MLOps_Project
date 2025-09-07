26dc45e9f07931841f8240a8de23d06f82f7e472
Adarsh63 <81111217+Adarsh63@users.noreply.github.com>
2025-09-07 13:01:11 +0530

    MLFLow

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -2,7 +2,7 @@ from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
-                                            DataTransformationConfig,ModelTrainerConfig)
+                                            DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig)
 import os 
 import sys
 
@@ -51,7 +51,7 @@ class ConfigurationManager:
                 root_dir=config.root_dir,
                 STATUS_FILE=config.STATUS_FILE,
                 unzip_data_dir = config.unzip_data_dir,
-                all_schema=schema,
+                all_schema=schema
             )
 
             return data_validation_config
@@ -87,3 +87,24 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.Salary,
+            mlflow_uri="https://dagshub.com/Adarsh63/MLOps_Project.mlflow",
+           
+        )
+
+        return model_evaluation_config
